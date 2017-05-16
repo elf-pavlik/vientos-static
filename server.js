cc8fcc9063db40ec39044b9917f57a1076935cfb
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-05-15 20:31:54 -0500

    static HTTP/2 server

---
+++ server.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const http2 = require('http2')
+const Path = require('path')
+const Hapi = require('hapi')
+const Inert = require('inert')
+
+const PORT = process.env.PWA_PORT || 8080
+const PWA_DIRNAME = process.env.PWA_DIRNAME || 'vientos-pwa'
+
+const httpServerOptions = {
+  key: fs.readFileSync(process.env.TLS_KEY_PATH),
+  cert: fs.readFileSync(process.env.TLS_CERT_PATH)
+}
+
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, PWA_DIRNAME)
+      }
+    }
+  }
+})
+
+const connectionOptions = { port: PORT }
+if (httpServerOptions.key && httpServerOptions.cert) {
+  connectionOptions.listener = http2.createServer(httpServerOptions)
+}
+server.connection(connectionOptions)
+
+server.register(Inert, () => {})
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      index: true
+    }
+  }
+})
+
+server.ext('onPostHandler', function (request, reply) {
+  const response = request.response
+  if (response.isBoom &&
+        response.output.statusCode === 404) {
+    return reply.file('index.html')
+  }
+
+  return reply.continue()
+})
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+
+  console.log('Server running at:', server.info.uri)
+})
